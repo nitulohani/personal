395b5abc0ea8137c85871251a37717f68f112da8
NituL <Nitu..lohani@mortgagebrain.co.uk>
2022-08-01 12:53:39 +0100

    check

--- splitProg.js
+++ splitProg.js
@@ -1,6 +1,6 @@
-let text="Hello my name is ";
+let text="Hello my name is "+  '"'+"Naveen" +'"';
 let textNi= "Hello Nitu,Hello Nitin";
-let outNi=textNi.split();
+let outNi=textNi.split(",",1);
 
-console.log(text +  '"'+"Naveen" +'"');
+console.log(text );
 console.log(outNi);
