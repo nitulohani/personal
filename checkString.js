96183c6c7ea662551ab1261228758d280a8f2cd1
NituL <Nitu..lohani@mortgagebrain.co.uk>
2022-08-01 09:25:09 +0100

    check string

---
+++ checkString.js
@@ -0,0 +1,10 @@
+let text="Hello Nitu,Hello Nitin";
+
+if (text.includes("Hello Nitu,Hello Nitin from plot 1"))
+{
+    console.log("Details matched and Navu is happy")
+
+}
+else{
+    console.log("more assignments");
+}
