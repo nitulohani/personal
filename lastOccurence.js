2a50ef29d7bd90cdb00fb9f07d70ee7f2c81d805
NituL <Nitu..lohani@mortgagebrain.co.uk>
2022-08-02 09:47:56 +0100

    check

--- lastOccurence.js
+++ lastOccurence.js
@@ -2,5 +2,5 @@ const paragraph = 'Hello Nitu,Hello Nitin from plot 1';
 
 const searchTerm = 'Hello';
 
-console.log(`The index of the first "${searchTerm}" from the end is ${paragraph.lastIndexOf(searchTerm)}`);
+console.log(`The index of the first "${searchTerm}" from the start is ${paragraph.indexOf(searchTerm)}`);
 
--- splitProg.js
+++ splitProg.js
@@ -1,6 +1,21 @@
 let text="Hello my name is "+  '"'+"Naveen" +'"';
 let textNi= "Hello Nitu,Hello Nitin";
-let outNi=textNi.split("",1);
+let indexText=textNi.indexOf(",")
+let splitTextFirst="";
+let splitTextSecond="";
 
-console.log(text );
-console.log(outNi);
+
+
+for(let i=0;i<indexText;i++){
+    splitTextFirst += textNi[i];
+}
+console.log(splitTextFirst);
+
+for(let y=indexText+1;y<textNi.length;y++){
+    splitTextSecond += textNi[y];
+}
+console.log(splitTextSecond);
+
+let addedText=splitTextFirst + "#" +splitTextSecond;
+
+console.log(addedText);
